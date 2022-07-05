3ea723961fb4dc937ac9f57468bcfc357927668b
InTr0p <christianolivare01@gmail.com>
2022-07-05 12:44:01 +0000

    Todolist with fetch :)

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,52 @@
 import React, { useState } from "react";
 import Form from "./component/form.jsx";
 import Home from "./component/home.jsx";
+import {useEffect} from 'react';
 
 function App() {
-	const [taskList, setTaskList] = useState([]);
+	const [taskList, setTaskList ] = useState([]);
+
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/InTr0p")
+			.then((response) => response.json())
+			.then((data) => setTaskList(data));
+	}, []);
 
 	const newTaskList = (task) => {
-		setTaskList([task, ...taskList]);
+		const createdTask = {
+			done: false,
+			label: task,
+		};
+		setTaskList([createdTask, ...taskList]);
+		updateList([createdTask, ...taskList]);
 	};
 
 	const erase = (id) => {
 		const listOrder = taskList.filter((e, index) => index !== id);
 		setTaskList(listOrder);
+		updateList(listOrder);
+	};
+
+	const updateList = (newTaskist) => {
+		var myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+
+		var raw = JSON.stringify(newTaskist);
+     
+		var requestOptions = {
+			method: "PUT",
+			headers: myHeaders,
+			body: raw,
+			redirect: "follow",
+		};
+
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/InTr0p",
+			requestOptions
+		)
+			.then((response) => response.text())
+			.then((result) => console.log(result))
+			.catch((error) => console.log("error", error));
 	};
 
 	return (
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,7 +10,7 @@ const Home = ({ erase, id, task }) => {
 			<div className="row d-flex">
 				<div className="col-sm-12 col-md-8 col-lg-8 mx-auto bg-dark rounded mt-1 d-flex justify-content-sm-between">
 					<div className="d-inline-block mt-2">
-						<span className="text-info p-2 fs-5">{task}</span>
+						<span className="text-info p-2 fs-5">{task.label}</span>
 					</div>
 					<div className="mt-1 d-inline-block mb-1">
 						<span className="text-danger fs-4 erase" onClick={deleteTask}>
